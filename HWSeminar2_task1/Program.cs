38d617cb7d05661092721d29d41da82f0e43adb2
Andrei <andrei.miridonov163@gmail.com>
2022-10-20 22:31:23 +0500

    Первая задача ДЗ к Семинару 2

---
+++ HWSeminar2_task1/Program.cs
@@ -0,0 +1,23 @@
+﻿//Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+//456 -> 5
+//782 -> 8
+//918 -> 1
+
+
+int Seconddigit(int digit)
+{
+    int Seconddigit = digit / 10 % 10;
+    return Seconddigit;
+}
+
+
+Console.WriteLine("Введите 3-х значное число");
+int digit = int.Parse (Console.ReadLine());
+
+int Secondnumber = Seconddigit(digit);
+
+Console.Write("Вторая цифра числа:");
+Console.WriteLine (Secondnumber);
+
+
