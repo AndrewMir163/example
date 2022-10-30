b39b4f91a844a9e3d9591aeaea5387483df8444b
Andrei <andrei.miridonov163@gmail.com>
2022-10-30 10:58:43 +0500

    13 задача, недорешенная

---
+++ HWSeminar2_task13/Program.cs
@@ -0,0 +1,20 @@
+﻿//Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+//645 -> 5
+
+//78 -> третьей цифры нет
+
+//32679 -> 6
+
+
+
+Console.WriteLine("Введите число");
+
+int a = int.Parse (Console.ReadLine()!);
+string str = a.ToString();
+int[] b = new int[str.Length];
+for( int i=0; i< str.Length; i++)
+{
+     b[i] = int.Parse(str);
+     Console.WriteLine(b);
+}
