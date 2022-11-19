b4ca9b9dadf199e14471da1ef7d1cad345438958
Andrei <andrei.miridonov163@gmail.com>
2022-11-19 17:52:08 +0500

    задание 41

---
+++ Seminar6_task41/Program.cs
@@ -0,0 +1,40 @@
+﻿/* Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+0, 7, 8, -2, -2 -> 2
+
+1, -7, 567, 89, 223-> 3 */
+
+/* Console.Write("Введите любые числа (через пробел): ");
+
+int[] array = Array.ConvertAll(Console.ReadLine().Split(),Convert.ToInt32);
+int count = 0;
+ 
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i] > 0)
+    {
+        count++;
+    }
+}
+ 
+Console.WriteLine($"Чисел > 0: {count}");
+ */
+
+Console.Write("Введите любые числа : ");
+
+string[] number = Convert.ToString( Console.ReadLine());
+
+int size = number.Length;
+
+void ArrayPrint(string[]number)
+{
+    for (int i = 0; i< size; i++)
+    {
+        Console.Write("," (number[i]));
+    }
+}
+
+Console.WriteLine(ArrayPrint);
+
+
+
