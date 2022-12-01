a8a76f7970eabc02bbdcf3f35d24b5cc7ae90f42
Andrei <andrei.miridonov163@gmail.com>
2022-12-01 22:37:51 +0500

    задание 62

--- Seminar9_task62/Program.cs
+++ Seminar9_task62/Program.cs
@@ -7,32 +7,35 @@
 
 
 
-Console.WriteLine($"Задача 62: Заполните спирально массив 4 на 4.");
-
 int n = 4;
-int[,] sqareMatrix = new int[n, n];
+int[,] SpiralMatrix = new int[n, n];
 
 int temp = 1;
 int i = 0;
 int j = 0;
 
-while (temp <= sqareMatrix.GetLength(0) * sqareMatrix.GetLength(1))
+
+
+while (temp <= SpiralMatrix.GetLength(0) * SpiralMatrix.GetLength(1))
 {
-  sqareMatrix[i, j] = temp;
+  SpiralMatrix[i, j] = temp;
   temp++;
-  if (i <= j + 1 && i + j < sqareMatrix.GetLength(1) - 1)
+  if (i <= j + 1 && i + j < SpiralMatrix.GetLength(1) - 1)
     j++;
-  else if (i < j && i + j >= sqareMatrix.GetLength(0) - 1)
+  else if (i < j && i + j >= SpiralMatrix.GetLength(0) - 1)
     i++;
-  else if (i >= j && i + j > sqareMatrix.GetLength(1) - 1)
+  else if (i >= j && i + j > SpiralMatrix.GetLength(1) - 1)
     j--;
   else
     i--;
+
 }
 
-WriteArray(sqareMatrix);
 
-void WriteArray (int[,] array)
+Console.WriteLine("Спиральный массив:");
+PrintArray(SpiralMatrix);
+
+void PrintArray (int[,] array)
 {
   for (int i = 0; i < array.GetLength(0); i++)
   {
