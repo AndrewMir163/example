0a76059636d622345b3d2ee6c3107d740f3fc5d7
Andrei <andrei.miridonov163@gmail.com>
2022-12-01 22:38:36 +0500

    задание 60

--- Seminar8_task60/Program.cs
+++ Seminar8_task60/Program.cs
@@ -10,7 +10,7 @@
 
 
 
-Console.WriteLine("nВведите размер массива X x Y x Z:");
+Console.WriteLine("Введите размер массива X x Y x Z:");
 
 int x = InputNumbers("Введите X: ");
 int y = InputNumbers("Введите Y: ");
